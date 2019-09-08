e53bb4fab6cd0f37b3047c432ad8fe6b28d99edd
Todd Burry <todd@vanillaforums.com>
2019-09-08 15:18:17 -0400

    Remove the name from the route collection

--- src/RouteCollection.php
+++ src/RouteCollection.php
@@ -10,16 +10,36 @@ namespace Garden\Route;
 final class RouteCollection {
     use MiddlewareAwareTrait;
 
-    public function map(string $name, string $method, string $path, $handler): Route {
+    public function map(string $method, string $path, $handler): Route {
         throw new NotImplementedException(__METHOD__);
     }
 
-    public function get(string $name, string $path, $handler): Route {
-        throw new NotImplementedException(__METHOD__);
+    public function get(string $path, $handler): Route {
+        return $this->map('GET', $path, $handler);
     }
 
-    public function post(string $name, string $path, $handler): Route {
-        throw new NotImplementedException(__METHOD__);
+    public function post(string $path, $handler): Route {
+        return $this->map('POST', $path, $handler);
+    }
+
+    public function put(string $path, $handler): Route {
+        return $this->map('PUT', $path, $handler);
+    }
+
+    public function patch(string $path, $handler): Route {
+        return $this->map('PATCH', $path, $handler);
+    }
+
+    public function delete(string $path, $handler): Route {
+        return $this->map('DELETE', $path, $handler);
+    }
+
+    public function head(string $path, $handler): Route {
+        return $this->map('HEAD', $path, $handler);
+    }
+
+    public function options(string $path, $handler): Route {
+        return $this->map('OPTIONS', $path, $handler);
     }
 
     public function group(string $prefix, callable $loader): RouteCollection {
